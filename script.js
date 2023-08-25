e7a1cef117f44b5e67f29fa164b4e3a85e8abffb
RileyT420 <90624488+RileyT420@users.noreply.github.com>
2023-08-25 18:01:12 +0000

    .

--- script.js
+++
@@ -1,5 +0,0 @@
-const buyButton = document.getElementById("buy-button");
-
-buyButton.addEventListener("click", () => {
-  alert("Thank you for choosing Awesome Product! Your purchase is successful.");
-});
